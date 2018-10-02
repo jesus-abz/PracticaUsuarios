551b89e414cda1354c792a7e64d6617d1986130f
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-10-02 08:50:35 -0700

    Agregar archivos de proyecto.

---
+++ PracticaUsuarios/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaUsuarios
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           PanelConfig.Children.Clear();
+           switch (cbUser.SelectedIndex)
+           {
+               case 0:
+                   PanelConfig.Children.Add(new ControlAlumno());
+                   break;
+               case 1:
+                   PanelConfig.Children.Add(new ControlProfesor());
+                   break;
+           }
+    }
+}
